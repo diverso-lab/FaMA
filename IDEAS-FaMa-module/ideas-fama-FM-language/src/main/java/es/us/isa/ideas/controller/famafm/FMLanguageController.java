7b785c97b7e4fedb6eae1d9d722200530bdc0dca
Jesus <mastermate@gmail.com>
2014-05-07 11:04:38 +0200

    final fixes for fama ideas branch

---
+++ IDEAS-FaMa-module/ideas-fama-FM-language/src/main/java/es/us/isa/ideas/controller/famafm/FMLanguageController.java
@@ -0,0 +1,60 @@
+package es.us.isa.ideas.controller.famafm;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import es.us.isa.ideas.common.AppResponse;
+import es.us.isa.ideas.common.AppResponse.Status;
+import es.us.isa.ideas.module.controller.BaseLanguageController;
+
+
+@Controller
+@RequestMapping("/language")
+public class FMLanguageController extends BaseLanguageController {
+
+	@RequestMapping(value = "/operation/{id}/execute", method = RequestMethod.POST)
+	@ResponseBody
+	public AppResponse executeOperation(String id, String content, String fileUri) {
+		
+		AppResponse response = null;
+		
+		return response;
+	}
+
+	@RequestMapping(value = "/format/{format}/checkLanguage", method = RequestMethod.POST)
+	@ResponseBody
+	public AppResponse checkLanguage(String id, String content, String fileUri) {
+		
+		AppResponse appResponse = new AppResponse();
+		
+		boolean problems = false;
+
+		//System.out.println("CheckSyntax: " + res );
+		appResponse.setFileUri(fileUri);
+		
+		if (problems)
+			appResponse.setStatus(Status.OK_PROBLEMS);
+		else
+			appResponse.setStatus(Status.OK);
+		
+		
+		return appResponse;
+	}
+
+	@RequestMapping(value = "/convert", method = RequestMethod.POST)
+	@ResponseBody
+	public AppResponse convertFormat(String currentFormat, String desiredFormat, String fileUri, String content) {
+		AppResponse appResponse = new AppResponse();
+		
+		return appResponse;
+	}
+	
+	// Analysis operations:
+	
+	
+
+}
