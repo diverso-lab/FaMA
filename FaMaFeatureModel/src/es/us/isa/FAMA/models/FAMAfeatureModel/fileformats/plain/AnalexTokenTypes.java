e1594eebeacef832b26d283eda0b20ec16b3872a
José A. Galindo <malawito@Joses-MacBook-Air.local>
2015-10-12 00:12:08 +0200

    adding tutorial to fama

---
+++ FaMaFeatureModel/src/es/us/isa/FAMA/models/FAMAfeatureModel/fileformats/plain/AnalexTokenTypes.java
@@ -0,0 +1,55 @@
+// $ANTLR : "Analex.g" -> "Analex.java"$
+
+	package es.us.isa.FAMA.models.FAMAfeatureModel.fileformats.plain;	
+
+public interface AnalexTokenTypes {
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int FEATURE_MODEL = 4;
+	int FEATURE = 5;
+	int FEATURES = 6;
+	int CONSTRAINTS = 7;
+	int CONSTRAINT = 8;
+	int DOMINIO = 9;
+	int DEF_VALUE = 10;
+	int NULL_VALUE = 11;
+	int RELACION = 12;
+	int CARDINALIDAD = 13;
+	int RELACIONES = 14;
+	int RANGO = 15;
+	int LITERAL = 16;
+	int RANGOS = 17;
+	int VALORES = 18;
+	int ENUM = 19;
+	int MENOS_UNARIO = 20;
+	int CORCHETE_ABRIR = 21;
+	int CORCHETE_CERRAR = 22;
+	int SECCION_RELACIONES = 23;
+	int DOSPUNTOS = 24;
+	int PyC = 25;
+	int IDENT = 26;
+	int LIT_ENTERO = 27;
+	int COMA = 28;
+	int LLAVE_ABRIR = 29;
+	int LLAVE_CERRAR = 30;
+	int SECCION_CONSTRAINTS = 31;
+	int EXCLUDES = 32;
+	int REQUIRES = 33;
+	int SALTO = 34;
+	int BLANCO = 35;
+	int LETRA = 36;
+	int BARRA_BAJA = 37;
+	int GUION = 38;
+	int DIGITO = 39;
+	int COMILLA = 40;
+	int PUNTO = 41;
+	int ALMOADILLA = 42;
+	int LIT_STRING = 43;
+	int NUMERO = 44;
+	int MAS = 45;
+	int PARENTESIS_ABRIR = 46;
+	int PARENTESIS_CERRAR = 47;
+	int VIRGULA = 48;
+	int VERSION = 49;
+	int COMENT_LIN = 50;
+}
